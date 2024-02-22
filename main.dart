e73bdab5373b16c7bd9b30dbdd069303163921e9
markerson7 <markersondflomo@gmil.com>
2024-02-22 19:03:02 +0200

    my first dart code

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  //declaring variables
+
+  String name = "Markerson D Flomo"; 
+  String address = "Kigali, Rwanda";
+  num age = 20;
+  num height = 5.9;
+  bool isMarried = false;
+
+  //printing variables
+
+  print("Hello, my name is $name and I'm $age years old, I am $height tall, and live in $address, and married status is $isMarried");
+  
+}
